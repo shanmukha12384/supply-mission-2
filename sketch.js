f18a502b7bac1254a5da4da1e1e96e14b5fe13a2
shanmukha12384 <71809117+shanmukha12384@users.noreply.github.com>
2020-10-17 17:52:28 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,78 +1,5 @@
-var helicopterIMG, helicopterSprite, packageSprite,packageIMG,box1Sprite,box2Sprite,box3Sprite;
-var packageBody,ground
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
-
-function preload()
-{
-	helicopterIMG=loadImage("helicopter.png")
-	packageIMG=loadImage("package.png")
-}
-
-function setup() {
-	createCanvas(800, 700);
-	rectMode(CENTER);
-	
-
-	packageSprite=createSprite(width/2, 80, 10,10);
-	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
-
-	helicopterSprite=createSprite(width/2, 200, 10,10);
-	helicopterSprite.addImage(helicopterIMG)
-	helicopterSprite.scale=0.6
-
-	groundSprite=createSprite(width/2, height-35, width,10);
-	groundSprite.shapeColor=color(255)
-
-	box1Sprite = createSprite(350,610,20,100);
-
-	box2Sprite = createSprite(400,650,100,20);
-
-	box3Sprite = createSprite(450,610,20,100);
-	box3Sprite=color(255);
-
-	engine = Engine.create();
-	world = engine.world;
-
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1, isStatic:true});
-	World.add(world, packageBody);
-	
-
-	//Create a Ground
-	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
- 	World.add(world, ground);
-
-
-	Engine.run(engine);
-
-	packageOption={
-        restitution : 0.2
-    }
-  
-}
-
-
-function draw() {
-  rectMode(CENTER);
-  background("black");
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
-  drawSprites();
- 
-}
-
-function keyPressed() 
-{ if (keyCode === DOWN_ARROW) { Matter.Body.setStatic(packageBody,false); } }
-
-
-
-
-
 var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground
+var packageBody,ground,wall1,wall2,wall3,wall1Body
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
@@ -88,13 +15,8 @@ function setup() {
 	createCanvas(800, 700);
 	rectMode(CENTER);
 	
-	var packageSprite_options ={
-       restitution: 800
-    }
-   
-    
-  
-	packageSprite=createSprite(400, 200, 10,packageSprite_options);
+
+	packageSprite=createSprite(200, 200, 10,10);
 	packageSprite.addImage(packageIMG)
 	packageSprite.scale=0.2
 
@@ -105,6 +27,14 @@ function setup() {
 	groundSprite=createSprite(width/2, height-35, width,10);
 	groundSprite.shapeColor=color(255)
 
+	wall1 = createSprite(width/2,650,200,20);
+	wall1.shapeColor = "red";
+
+	wall2 = createSprite(300,610,20,100);
+	wall2.shapeColor = "red";
+
+	wall3 = createSprite(500,610,20,100);
+	wall3.shapeColor = "red";
 
 	engine = Engine.create();
 	world = engine.world;
@@ -117,6 +47,8 @@ function setup() {
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
  	World.add(world, ground);
 
+	wall1Body = Bodies.rectangle(width/2,650,200,50,{isStatic:true});
+	World.add(world, wall1Body);
 
 	Engine.run(engine);
 
@@ -133,21 +65,21 @@ function draw() {
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
 
-if(keyDown("space")){
+  if(keyDown("DOWN_ARROW")){
 	
-		Matter.Body.setStatic(packageBody, false);
-	
+	Matter.Body.setStatic(packageBody, false);
+		packageBody.restitution = 0.8;
 
 
 
 
 }
 
- drawSprites();
+  drawSprites();
  
 }
 
 
 
 
-	
+	  
